629e70a6b63d2fc0e4b76b0cef9fdc95c6a76107
Flavien Vernier <flavien.vernier@gmail.com>
2022-04-24 14:57:46 +0200

    add a javadoc for jenkins test

--- src/timer/Timer.java
+++ src/timer/Timer.java
@@ -3,14 +3,14 @@ package timer;
 import java.util.Iterator;
 
 public interface Timer extends Iterator<Integer>{
-	/*
-	 * return the delay time
-	 * @see java.util.Iterator#next()
-	 */
-	public Integer next();
-	/*
-	 * return the delay time
-	 * @see java.util.Iterator#next()
-	 */
-	//public Integer next(int now);
+    /**
+     * return the delay time
+     * @see java.util.Iterator#next()
+     */
+    public Integer next();
+    /**
+     * return the delay time
+     * @see java.util.Iterator#next()
+     */
+    //public Integer next(int now);
 }
